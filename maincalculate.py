0042aa7bcc1a57844066c63857a45c3f47ea0f64
StanokBigRus <136271239+StanokBigRus@users.noreply.github.com>
2023-06-12 01:55:41 +0300

    Add files via upload

--- maincalculate.py
+++ maincalculate.py
@@ -1,19 +1,20 @@
-from math import *
+from math import * #Подключаем математический модуль
 
 
 print("Hello, My dear Brothers. Today I am pleased to present you a mini-calculator.\n"
       "You can use the whole math set of the development environment.\n"
-      "If you enter incorrect data, the application will notify you. The application will prompt you to enter a new expression each time.\n"
+      "If you enter incorrect data, the application will notify you. The application will prompt you to enter a new expression each time.\n"      
+      "Thank you for your creativity and for the best implementation of The Witcher. I hope that I can join your ranks, because this is my dream. Witcher.\n\n"
       "Type 'exit' or 'quit' to exit the program.\n"
       "")
 while True:
    try:
       inexp = input('Enter an expression :\n')
       inexp = inexp.lower()
-      if inexp.replace(" ","") in {"quit", "exit"}:
+      if inexp.replace(" ","") in {"quit", "exit"}: #Переводим в нижний регистр и проверяем на команды выхода(теперь нам не важно кол-во пробелов в командах и регистр вводимый пользователем)
          raise SystemExit()
-      print(eval(inexp))
-   except (TypeError, NameError, SyntaxError, ZeroDivisionError):
+      print(eval(inexp)) #Вычисляем выражение
+   except (TypeError, NameError, SyntaxError, ZeroDivisionError): #Проверка на неккоректный ввод
       print("Expression entered incorrectly")
 
 
